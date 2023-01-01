720f6f16e85bc0ac0a5db2c12bc015b7268f1f6c
Amy9788 <eaponchik@gmail.com>
2023-01-01 13:52:23 -0600

    Adding chapter 2

---
+++ Chapter_2/print_input_from_user.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 0;
+    cout << "Please enter a number: ";
+    cin >> x;
+    cout << "Your number is: " << x << " \n";
+    return 0;
+}
